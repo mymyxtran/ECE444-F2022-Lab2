b377026bbec855a6d5f926c5d058c5fe2354f6e9
Mymy Tran <mymytraan@gmail.com>
2022-09-28 00:52:44 -0400

    Chapter 3: Custom error pages (3c)

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template 
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/') 
 def index(): 
-    return render_template('index.html', time=1)
+    return render_template('index.html')
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name) #render_template function from flask
 
-# bootstrap = Bootstrap(app)
+@app.route('/500') # idk the actual route yet
+def err500():
+    return render_template('500.html')
+
+@app.route('/400')
+def err400():
+    return render_template('400.html')
