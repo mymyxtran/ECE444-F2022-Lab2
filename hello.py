53624b85076f15432837089aef8f6f81d9d675e4
Mymy Tran <mymytraan@gmail.com>
2022-09-28 09:44:43 -0400

    Chapter 4: Redirects and user sessions (4b)

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, session, redirect, url_for 
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -10,28 +8,24 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ABCDE'
 bootstrap = Bootstrap(app)
-moment = Moment(app) # init the flask-moment
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) # The StringField class represents an HTML <input> element with a type="text" attribute. And we have an optional validator.
     submit = SubmitField('Submit')
 
-
 @app.route('/') 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']) 
 def index(): 
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name) #render_template function from flask
+    form = NameForm() 
+    if form.validate_on_submit(): 
+        session['name'] = form.name.data 
+        return redirect(url_for('index')) 
+    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.errorhandler(500) # idk the actual route yet
+@app.errorhandler(500)
 def internal_server_error():
     return render_template('500.html')
 
 @app.errorhandler(404)
 def page_not_found():
     return render_template('404.html')
-
