4e8883431d826021a4ab3f584461e3117ce6facc
Mymy Tran <mymytraan@gmail.com>
2022-09-28 10:30:06 -0400

    Chapter 4: Add one more field for email address verify is Email and has substring utoronto

--- hello.py
+++ hello.py
@@ -3,14 +3,26 @@ from flask_bootstrap import Bootstrap
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ABCDE'
 bootstrap = Bootstrap(app)
 
+class VerifyIsUofTEmail(object):
+    def __init__(self, email_substring = 'utoronto',  message=None):
+        self.email_substring = email_substring
+        if not message:
+            message = u'Please use your UofT email.'
+        self.message = message
+
+    def __call__(self, form, field):
+        if field.data != None and self.email_substring not in field.data:
+            raise ValidationError(self.message)
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) # The StringField class represents an HTML <input> element with a type="text" attribute. And we have an optional validator.
+    email = StringField('What is your UofT email address?', validators=[Email(), VerifyIsUofTEmail()])
     submit = SubmitField('Submit')
 
 @app.route('/') 
